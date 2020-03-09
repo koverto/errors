75cbb453253ddd9421b33eb6a0fab41b151a7c70
Jesse B. Hannah <jesse@jbhannah.net>
2020-03-09 16:17:25 -0700

    Documentation and linting fixes

--- errors.go
+++ errors.go
@@ -1,3 +1,5 @@
+// Package errors provides common custom error types for sharing across
+// microservices, wrapping github.com/micro/go-micro/v2/errors.
 package errors
 
 import (
@@ -6,48 +8,55 @@ import (
 	"github.com/micro/go-micro/v2/errors"
 )
 
-type Error = errors.Error
-
+// BadRequest generates a 400 error.
 func BadRequest(id, format string, a ...interface{}) error {
 	return errors.BadRequest(id, format, a...)
 }
 
-func Conflict(id, format string, a ...interface{}) error {
-	return errors.Conflict(id, format, a...)
+// Unauthorized generates a 401 error.
+func Unauthorized(id, format string, a ...interface{}) error {
+	return errors.Unauthorized(id, format, a...)
 }
 
+// Forbidden generates a 403 error.
 func Forbidden(id, format string, a ...interface{}) error {
 	return errors.Forbidden(id, format, a...)
 }
 
-func InternalServerError(id, format string, a ...interface{}) error {
-	return errors.InternalServerError(id, format, a...)
-}
-
-func MethodNotAllowed(id, format string, a ...interface{}) error {
-	return errors.MethodNotAllowed(id, format, a...)
-}
-
+// NotFound generates a 404 error.
 func NotFound(id, format string, a ...interface{}) error {
 	return errors.NotFound(id, format, a...)
 }
 
-func NotImplemented(id string) *Error {
+// MethodNotAllowed generates a 405 error.
+func MethodNotAllowed(id, format string, a ...interface{}) error {
+	return errors.MethodNotAllowed(id, format, a...)
+}
+
+// Timeout generates a 408 error.
+func Timeout(id, format string, a ...interface{}) error {
+	return errors.Timeout(id, format, a...)
+}
+
+// Conflict generates a 409 error.
+func Conflict(id, format string, a ...interface{}) error {
+	return errors.Conflict(id, format, a...)
+}
+
+// InternalServerError generates a 500 error.
+func InternalServerError(id, format string, a ...interface{}) error {
+	return errors.InternalServerError(id, format, a...)
+}
+
+// NotImplemented generates a 501 error.
+func NotImplemented(id string) *errors.Error {
 	code := http.StatusNotImplemented
 	text := http.StatusText(code)
 
-	return &Error{
+	return &errors.Error{
 		Id:     id,
 		Code:   int32(code),
 		Detail: text,
 		Status: text,
 	}
-}
-
-func Timeout(id, format string, a ...interface{}) error {
-	return errors.Timeout(id, format, a...)
-}
-
-func Unauthorized(id, format string, a ...interface{}) error {
-	return errors.Unauthorized(id, format, a...)
 }
--- errors_test.go
+++ errors_test.go
@@ -1,29 +1,35 @@
 package errors
 
 import (
+	"net/http"
 	"testing"
+
+	"github.com/micro/go-micro/v2/errors"
 )
 
 func TestNotImplemented(t *testing.T) {
 	type args struct {
 		id string
 	}
+
 	tests := []struct {
 		name string
 		args args
-		want Error
+		want errors.Error
 	}{
-		{"NotImplemented error", args{"com.koverto.errors.test"}, Error{
+		{"NotImplemented error", args{"com.koverto.errors.test"}, errors.Error{
 			Id:     "com.koverto.errors.test",
-			Code:   501,
+			Code:   http.StatusNotImplemented,
 			Detail: "Not Implemented",
 			Status: "Not Implemented",
 		}},
 	}
+
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NotImplemented(tt.args.id); got.Error() != tt.want.Error() {
-				t.Errorf("NotImplemented() error = %v, want %v", *got, tt.want)
+		test := tt
+		t.Run(test.name, func(t *testing.T) {
+			if got := NotImplemented(test.args.id); got.Error() != test.want.Error() {
+				t.Errorf("NotImplemented() error = %v, want %v", *got, test.want)
 			}
 		})
 	}
